4deccbcc4f489fd211b726f7b4164da7eabc6fcd
Timon <dackeldu007@gmail.com>
2022-05-11 16:25:58 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
 
-function App() {
+const App = () => {
+  let images = [];
+
+  for (let i = 0; i < 12; i++) {
+    images.push({
+      src: `https://pic.re/image/${Math.random()}`
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel images={images} style={{ height: 500, width: 800 }} />
   );
-}
+};
 
 export default App;
